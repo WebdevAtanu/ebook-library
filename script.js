d336dbae4d17291ac839db375988662d38b8c0c8
Atanu <atanum991@gmail.com>
2024-03-02 11:00:19 +0530

    Initial

---
+++ script.js
@@ -0,0 +1,231 @@
+let authors = {
+    "Adrish": [{
+        "name": "অসহ্য সাসপেন্স 1",
+        "src": "https://drive.google.com/file/d/1mjlEdmylYqlll3DezD3jYNGlTYr0r_MU/view?usp=drive_link",
+        "cover": "cover/adrish/a1.png"
+    }, {
+        "name": "অসহ্য সাসপেন্স 2",
+        "src": "https://drive.google.com/file/d/1pjkj-wpp0u6usicOvB8VSyZEmVrHgHnd/view?usp=drive_link",
+        "cover": "cover/adrish/a2.png"
+    }, {
+        "name": "গোয়েন্দা ইন্দ্রনাথ রুদ্র সমগ্র",
+        "src": "https://drive.google.com/file/d/1V-jFUKKtEyVl_st4SlnPIgUoAYwAVVoa/view?usp=drive_link",
+        "cover": "cover/adrish/g.png"
+    }, {
+        "name": "প্রফেসর নাটবল্টু চক্র সংগ্রহ 1",
+        "src": "https://drive.google.com/file/d/1IIxEhwtSTxGTV8gUAsCI7gOgMnG2MdSk/view?usp=drive_link",
+        "cover": "cover/adrish/p1.png"
+    }, {
+        "name": "প্রফেসর নাটবল্টু চক্র সংগ্রহ 2",
+        "src": "https://drive.google.com/file/d/1Wg8wW0JdtdJt-BXvNcNksdXtnFFdsgLE/view?usp=drive_link",
+        "cover": "cover/adrish/p2.png"
+    }],
+    "Ajeo": [{
+        "name": "অ্যাডভেঞ্চার সমগ্র",
+        "src": "https://drive.google.com/file/d/10OL-CP-0S_eXf8VvelZixLD33TBR4OuD/view?usp=drive_link",
+        "cover": "cover/ajeo/a.png"
+    }, {
+        "name": "গল্প সংগ্রহ",
+        "src": "https://drive.google.com/file/d/1dy9vsaHcCTvIM5I9jIEvUEvIH6oUFbhn/view?usp=drive_link",
+        "cover": "cover/ajeo/g.png"
+    }],
+    "Anil": [{
+        "name": "ফ্রান্সিস সমগ্র 1",
+        "src": "https://drive.google.com/file/d/19qVFtl2g_XHUOP6rokNVjC79DRQXEmPh/view?usp=drive_link",
+        "cover": "cover/anil/f.png"
+    }, {
+        "name": "ফ্রান্সিস সমগ্র 2",
+        "src": "https://drive.google.com/file/d/12oQulcYb2gnjylKRkUs5AQTu7c1QGN1k/view?usp=drive_link",
+        "cover": "cover/anil/f.png"
+    }, {
+        "name": "ফ্রান্সিস সমগ্র 3",
+        "src": "https://drive.google.com/file/d/1e3rOs8Hxsaxw_80xD7tDGqFaeQRdUYbH/view?usp=drive_link",
+        "cover": "cover/anil/f.png"
+    }, {
+        "name": "ফ্রান্সিস সমগ্র 4",
+        "src": "https://drive.google.com/file/d/1dkwayxRqSpL0-sDpNjv9hD-6S1zISgLA/view?usp=drive_link",
+        "cover": "cover/anil/f.png"
+    }, {
+        "name": "ফ্রান্সিস সমগ্র 5",
+        "src": "https://drive.google.com/file/d/1E_Qwk7_Owcua62FYE9KhcHChTRX4HGCn/view?usp=drive_link",
+        "cover": "cover/anil/f.png"
+    }, {
+        "name": "ফ্রান্সিস সমগ্র 6",
+        "src": "https://drive.google.com/file/d/1yQT0tljpOvHGHy0MFXiKRPT2MnyDUpoN/view?usp=drive_link",
+        "cover": "cover/anil/f.png"
+    }, {
+        "name": "ফ্রান্সিস সমগ্র 7",
+        "src": "https://drive.google.com/file/d/1L7u7sbt8ha2LEPH5e2ISxrcgOkMNR8An/view?usp=drive_link",
+        "cover": "cover/anil/f.png"
+    }, {
+        "name": "ফ্রান্সিস সমগ্র 8",
+        "src": "https://drive.google.com/file/d/1wougRDGcKMosQxz8jHurkQ2PJ7lGpgXY/view?usp=drive_link",
+        "cover": "cover/anil/f.png"
+    }, {
+        "name": "ফ্রান্সিস সমগ্র 9",
+        "src": "https://drive.google.com/file/d/1Z-El2tjDeP1_5NbH2CWSrrbJOA98L2bk/view?usp=drive_link",
+        "cover": "cover/anil/f.png"
+    }],
+    "Anish": [{
+        "name": "অনিশের সেরা ১০১",
+        "src": "",
+        "cover": ""
+    }, {
+        "name": "পাতাল ঝর",
+        "src": "",
+        "cover": ""
+    }, {
+        "name": "বারোটি রহস্য উপন্যাস",
+        "src": "",
+        "cover": ""
+    }, {
+        "name": "বিশ্বের সেরা ভয়ঙ্কর ভূতের গল্প",
+        "src": "",
+        "cover": ""
+    }, {
+        "name": "ভয়পাতাল",
+        "src": "",
+        "cover": ""
+    }, {
+        "name": "ভূতনাথের ডায়েরি",
+        "src": "",
+        "cover": ""
+    }, {
+        "name": "রহস্য রোমাঞ্চ গোয়েন্দা পত্রিকার সেরা ১০০ গল্প",
+        "src": "",
+        "cover": ""
+    }, {
+        "name": "সেরা কল্পবিজ্ঞান",
+        "src": "",
+        "cover": ""
+    }],
+
+}
+
+
+
+
+
+function displayFunc() {
+    document.getElementById("mainContainer").replaceChildren();
+    let selectionValue = document.getElementById("selection").value;
+    let name = "";
+    console.log(selectionValue);
+    switch (selectionValue) {
+        case "Adrish":
+            document.getElementById("authorImg").setAttribute('src', 'author/adrish.jpg');
+            for (i = 0; i < authors.Adrish.length; i++) {
+                var div = document.createElement('div');
+                div.innerHTML = `
+                <div class="div">
+                <figure>
+					<a href="${authors.Adrish[i].src}" target="_blank">
+						<img src="${authors.Adrish[i].cover}" alt="">
+						<hr>
+						<figcaption>${authors.Adrish[i].name}</figcaption>
+					</a>
+					</figure>
+					</div>`
+                div.setAttribute('class', 'swiper-slide');
+                document.getElementById("mainContainer").appendChild(div);
+            }
+            break;
+        case "Ajeo":
+            document.getElementById("authorImg").setAttribute('src', 'image/logo.png');
+            for (i = 0; i < authors.Ajeo.length; i++) {
+                var div = document.createElement('div');
+                div.innerHTML = `
+                <div class="div">
+                <figure>
+					<a href="${authors.Ajeo[i].src}" target="_blank">
+						<img src="${authors.Ajeo[i].cover}" alt="">
+						<hr>
+						<figcaption>${authors.Ajeo[i].name}</figcaption>
+					</a>
+					</figure>
+					</div>`
+                div.setAttribute('class', 'swiper-slide');
+                document.getElementById("mainContainer").appendChild(div);
+            }
+            break;
+        case "Anil":
+            document.getElementById("authorImg").setAttribute('src', 'image/logo.png');
+            for (i = 0; i < authors.Anil.length; i++) {
+                var div = document.createElement('div');
+                div.innerHTML = `
+                <div class="div">
+                <figure>
+					<a href="${authors.Anil[i].src}" target="_blank">
+						<img src="${authors.Anil[i].cover}" alt="">
+						<hr>
+						<figcaption>${authors.Anil[i].name}</figcaption>
+					</a>
+					</figure>
+					</div>`
+                div.setAttribute('class', 'swiper-slide');
+                document.getElementById("mainContainer").appendChild(div);
+            }
+            break;
+        case "Anish":
+            for (i = 0; i < authors.Anish.length; i++) {
+                var div = document.createElement('div');
+                div.innerHTML = `
+                <div class="div">
+                <figure>
+					<a href="${authors.Anish[i].src}" target="_blank">
+						<img src="image/ex.jpg" alt="">
+						<hr>
+						<figcaption>${authors.Anish[i].name}</figcaption>
+					</a>
+					</figure>
+					</div>`
+                div.setAttribute('class', 'swiper-slide');
+                document.getElementById("mainContainer").appendChild(div);
+            }
+            break;
+    }
+
+}
+
+
+
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 5,
+    spaceBetween: 10,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 10
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 10
+        }
+    },
+
+    autoplay: {
+        delay: 5000,
+    },
+    // centeredSlides: true,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
